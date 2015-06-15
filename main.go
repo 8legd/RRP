3b1863abd0d5476c9aaeca3e821a6d05cfe1e212
8legd <git@8legd.com>
2015-06-15 23:30:44 +0100

    Continued work on initial release

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"os"
+
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web/middleware"
 
@@ -9,6 +13,12 @@ import (
 )
 
 func main() {
+
+	bind := os.Getenv("RRP_BIND")
+	if bind == "" {
+		log.Fatal("Missing RRP_BIND environmental variable")
+	}
+	// TODO usage error if missing RRP_BIND env var
 
 	// TODO use/write enhanced logging library?
 
@@ -21,8 +31,8 @@ func main() {
 
 	goji.Post("/batch/multipartmixed", batch.MultipartMixed)
 	goji.Post("/batch/debug", handlers.Debug)
-
 	// TODO support other batch requests e.g. AJAX support?
 
+	flag.Set("bind", bind)
 	goji.Serve()
 }
