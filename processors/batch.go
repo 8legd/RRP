2772a38e4b2982ba4f5268ff1a95c2ada3f6d862
8legd <git@8legd.com>
2015-09-02 20:55:05 +0100

    switch to http.Client (KISS)

--- processors/batch.go
+++ processors/batch.go
@@ -5,20 +5,22 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"strconv"
 	"sync"
 	"time"
 )
 
 var (
-	transport *http.Transport
+	// DefaultTimeout is the default timeout in seconds and is applied to all the requests contained in the batch
+	// (unless the batch request specifies a different value through the `x-rrp-timeout` header)
+	DefaultTimeout time.Duration
+	defaultClient  *http.Client
 )
 
 func init() {
-	// http.Transport is safe for concurrent use by multiple goroutines and for efficiency should only be created once and re-used
-	transport = &http.Transport{}
-	transport.Proxy = http.ProxyFromEnvironment
+	// http.Client is safe for concurrent use by multiple goroutines and for efficiency should only be created once and re-used
+	DefaultTimeout = time.Duration(20) * time.Second // Default timeout is 20 seconds, TODO should be configurable e.j flag
+	defaultClient = &http.Client{Timeout: DefaultTimeout}
 }
 
 type batchedRequest struct {
@@ -36,21 +38,11 @@ type BatchedResponse struct {
 	ProcessingDuration time.Duration
 }
 
-func checkUserAgent(request *http.Request) {
-	// Add default User-Agent of `RRP <version>` if none is specified in the request
-	// TODO remove hard coded version and set on build - need to setup our automated build first :)
-	if request.Header != nil {
-		if ua := request.Header["User-Agent"]; len(ua) == 0 {
-			request.Header.Set("User-Agent", "RRP 1.0.1")
-		}
-	}
-}
-
-func sendErrorResponse(sequence int, proto string, err error, timeout bool, startedProcessing time.Time, batchedResponses chan BatchedResponse) {
-	// Return an error response for any http transport errors - Status 400 (Bad Request)
+func sendErrorResponse(sequence int, proto string, err error, timeout time.Duration, startedProcessing time.Time, batchedResponses chan BatchedResponse) {
+	// Return an error response - Status 400 (Bad Request)
 	e := err
-	if timeout {
-		e = fmt.Errorf("request cancelled by timeout causing error: %s", err.Error())
+	if time.Since(startedProcessing) > timeout {
+		e = fmt.Errorf("request probably cancelled by timeout causing error: %s", err.Error())
 	}
 	errResponse := &http.Response{}
 	errResponse.Proto = proto
@@ -59,40 +51,42 @@ func sendErrorResponse(sequence int, proto string, err error, timeout bool, star
 	batchedResponses <- BatchedResponse{sequence, errResponse.Status, errResponse.Proto, &errResponse.Header, nil, time.Since(startedProcessing)}
 }
 
-// ProcessBatch sends a batch of HTTP requests using http.Transport.
+// ProcessBatch sends a batch of HTTP requests using http.Client.
 // Each request is sent concurrently in a seperate goroutine.
 // The HTTP responses are returned in the same sequence as their corresponding requests.
 func ProcessBatch(requests []*http.Request, timeout time.Duration) ([]*BatchedResponse, error) {
 	z := len(requests)
-	// Setup a buffered channel to queue up the requests for processing by individual HTTP Transport goroutines
+	// Setup a buffered channel to queue up the requests for processing by individual HTTP Client goroutines
 	batchedRequests := make(chan batchedRequest, z)
 	for i := 0; i < z; i++ {
 		batchedRequests <- batchedRequest{i, requests[i]}
 	}
 	// Close the channel - nothing else is sent to it
 	close(batchedRequests)
-	// Setup a second buffered channel for collecting the BatchedResponses from the individual HTTP Transport goroutines
+	// Setup a second buffered channel for collecting the BatchedResponses from the individual HTTP Client goroutines
 	batchedResponses := make(chan BatchedResponse, z)
 	// Setup a wait group so we know when all the BatchedRequests have been processed
 	var wg sync.WaitGroup
 	wg.Add(z)
 
-	// Start our individual HTTP Transport goroutines to process the BatchedRequests
+	var client *http.Client
+	if DefaultTimeout == timeout {
+		client = defaultClient
+	} else {
+		// create a non standard client for the request
+		// TODO keep a cache of most commonly used clients/timeout values so we can reuse them in the same way as we do for `defaultClient`
+		client = &http.Client{Timeout: timeout}
+	}
+
+	// Start our individual HTTP Client goroutines to process the BatchedRequests
 	for i := 0; i < z; i++ {
 		go func() {
 			defer wg.Done()
 			r := <-batchedRequests
 			startedProcessing := time.Now()
-			// Create a timer to timeout if processing the request takes too long
-			timedOut := false
-			timer := time.NewTimer(timeout)
-			go func() {
-				<-timer.C
-				timedOut = true
-				transport.CancelRequest(r.Request)
-			}()
-			checkUserAgent(r.Request)
-			response, err := transport.RoundTrip(r.Request)
+
+			response, err := client.Do(r.Request)
+
 			// Defer closing of underlying connection so it can be re-used
 			defer func() {
 				if response != nil && response.Body != nil {
@@ -100,42 +94,10 @@ func ProcessBatch(requests []*http.Request, timeout time.Duration) ([]*BatchedRe
 				}
 			}()
 			if err != nil {
-				sendErrorResponse(r.Sequence, r.Request.Proto, err, timedOut, startedProcessing, batchedResponses)
+				sendErrorResponse(r.Sequence, r.Request.Proto, err, timeout, startedProcessing, batchedResponses)
 				return
 			}
-			// TODO add support for all possible redirect status codes, see line 249 of https://golang.org/src/net/http/client.go
-			if response.StatusCode == 302 {
-				location := response.Header.Get("Location")
-				if location != "" {
-					redirectURL, err := url.Parse(location)
-					if err == nil {
-						if !redirectURL.IsAbs() { // handle relative URLs
-							redirectURL, err = url.Parse(r.Request.URL.Scheme + "://" + r.Request.Host + "/" + location)
-						}
-						queryString := ""
-						if len(redirectURL.Query()) > 0 {
-							queryString = "?" + redirectURL.Query().Encode()
-						}
-						// Close current connection before we issue redirect so it can be re-used
-						if response.Body != nil {
-							response.Body.Close()
-						}
-						redirect, err := http.NewRequest("GET", redirectURL.Scheme+"://"+redirectURL.Host+redirectURL.Path+queryString, nil)
-						if err == nil {
-							checkUserAgent(redirect)
-							response, err = transport.RoundTrip(redirect)
-							if err != nil {
-								sendErrorResponse(r.Sequence, r.Request.Proto, err, timedOut, startedProcessing, batchedResponses)
-								return
-							}
-						} else {
-							sendErrorResponse(r.Sequence, r.Request.Proto, err, timedOut, startedProcessing, batchedResponses)
-							return
-						}
-					}
-				}
-			}
-
+			// If there is no body to read we are done
 			if response.Body == nil {
 				batchedResponses <- BatchedResponse{r.Sequence, response.Status, response.Proto, &response.Header, nil, time.Since(startedProcessing)}
 				return
@@ -144,7 +106,7 @@ func ProcessBatch(requests []*http.Request, timeout time.Duration) ([]*BatchedRe
 			var buffy bytes.Buffer
 
 			// Read the response
-			// TODO chunkSize should be configurable (as per timeout)
+			// TODO chunkSize should be configurable (as per timeout above e.g. flag)
 			chunkSize := 16384
 			if response.ContentLength > 0 && response.ContentLength < int64(chunkSize) {
 				chunkSize = int(response.ContentLength)
@@ -154,7 +116,7 @@ func ProcessBatch(requests []*http.Request, timeout time.Duration) ([]*BatchedRe
 				lastReadLength, err := response.Body.Read(chunk)
 
 				if err != nil && err != io.EOF { // return on error in read
-					sendErrorResponse(r.Sequence, response.Proto, err, timedOut, startedProcessing, batchedResponses)
+					sendErrorResponse(r.Sequence, response.Proto, err, timeout, startedProcessing, batchedResponses)
 					return
 				}
 
@@ -166,7 +128,7 @@ func ProcessBatch(requests []*http.Request, timeout time.Duration) ([]*BatchedRe
 						_, err = buffy.Write(chunk)
 
 						if err != nil { // return on error in write to buffer
-							sendErrorResponse(r.Sequence, response.Proto, err, timedOut, startedProcessing, batchedResponses)
+							sendErrorResponse(r.Sequence, response.Proto, err, timeout, startedProcessing, batchedResponses)
 							return
 						}
 					}
@@ -178,7 +140,7 @@ func ProcessBatch(requests []*http.Request, timeout time.Duration) ([]*BatchedRe
 				_, err = buffy.Write(chunk) // write next chunk, and keep reading in loop
 
 				if err != nil { // return on error in write
-					sendErrorResponse(r.Sequence, response.Proto, err, timedOut, startedProcessing, batchedResponses)
+					sendErrorResponse(r.Sequence, response.Proto, err, timeout, startedProcessing, batchedResponses)
 					return
 				}
 			}
--- servers/goji/start.go
+++ servers/goji/start.go
@@ -26,12 +26,20 @@ func Start(bind string) {
 	custom := func(c *web.C, h http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			if r.Header != nil {
+
+				// Add default User-Agent of `RRP <version>` if none is specified in the request
+				// TODO remove hard coded version and set on build - need to setup our automated build first :)
+				if ua := r.Header["User-Agent"]; len(ua) == 0 {
+					r.Header.Set("User-Agent", "RRP 1.0.1")
+				}
+
 				// add `x-request-id` header if not present, as per heroku (https://devcenter.heroku.com/articles/http-request-id)
 				if ri := r.Header["x-request-id"]; len(ri) == 0 {
 					if reqID, ok := c.Env["reqID"].(string); ok { // goji provides a request id by default as part of its web context object
 						r.Header.Set("x-request-id", reqID)
 					}
 				}
+
 			}
 			h.ServeHTTP(w, r)
 		}
